a1cf9b16c3a93c4ab4d1ff4b4fba681e5832ca4c
wanghaibing <wanghaibing@shein.com>
2018-12-13 17:18:45 +0800

    添加多语言支持

---
+++ Web/app/Lang/Zh/Common.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'site_name'      => '36Photo',
+    'nav_discover'   => '发 现',
+    'nav_recommend'  => '推 荐',
+    'nav_photos'     => '照 片',
+    'nav_category'   => '分 类',
+    'nav_mark'       => '标 签',
+    'nav_color'      => '颜 色',
+    'nav_random'     => '随 机',
+    'nav_signup'     => '注 册',
+    'nav_signin'     => '登 录',
+    'nav_sub'        => '订 阅',
+    'nav_collection' => '专 题'
+];
--- Web/public/assets/js/app.js
+++ Web/public/assets/js/app.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     feather.replace();
+    // 切换语言
+    var lang = document.getElementById('lang');
+
+    lang.addEventListener('change', function() {
+        var lang_code = lang.options[lang.selectedIndex].value;
+        Cookies.set('Lang', lang_code, { expires: 7 });
+        location.reload();
+    });
 }); 
