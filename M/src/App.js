78f7efee1d7d22e0c94fca57395411af606110c8
wanghaibing <wanghaibing@shein.com>
2019-01-16 17:45:16 +0800

    移除 M 站

--- M/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Layout from "./Component/Layout"
-
-const App = props => (
-    <Layout />
-)
-
-export default App;
-
--- M/src/Component/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = props => (
-    <footer>
-        <h5>2018 Emage</h5>
-    </footer>
-)
-
-export default Footer
