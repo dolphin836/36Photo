3a7e02c5c5d2810b32c7b1b23e625984b9958113
whb <wanghaibing@shein.com>
2019-04-08 15:16:38 +0800

    some

--- Manage/app/Controller/User/PostAdd.php
+++ Manage/app/Controller/User/PostAdd.php
@@ -11,25 +11,7 @@ class PostAdd extends \Dolphin\Ting\Controller\Base
     {   
         $body = $request->getParsedBody();
 
-        $avatar = [
-            '',
-            'assets/images/faces/male/1.jpg',
-            'assets/images/faces/male/2.jpg',
-            'assets/images/faces/male/3.jpg',
-            'assets/images/faces/male/4.jpg',
-            'assets/images/faces/male/5.jpg',
-            'assets/images/faces/male/6.jpg',
-            'assets/images/faces/male/7.jpg',
-            'assets/images/faces/male/8.jpg',
-            'assets/images/faces/female/1.jpg',
-            'assets/images/faces/female/2.jpg',
-            'assets/images/faces/female/3.jpg',
-            'assets/images/faces/female/4.jpg',
-            'assets/images/faces/female/5.jpg',
-            'assets/images/faces/female/6.jpg',
-            'assets/images/faces/female/7.jpg',
-            'assets/images/faces/female/8.jpg'
-        ];
+        $avatar = 'assets/images/faces/male/1.jpg';
 
         $data = [
                 'uuid' => $this->random_code(),
@@ -39,7 +21,7 @@ class PostAdd extends \Dolphin\Ting\Controller\Base
                'phone' => trim($body['phone']),
                'email' => trim($body['email']),
             'password' => password_hash($body['password'], PASSWORD_DEFAULT),
-              'avatar' => $avatar[rand(1, 16)],
+              'avatar' => $avatar,
                 'sign' => trim($body['sign']),
                'group' => $body['group']
         ];
